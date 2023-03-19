6bb6a6228ca22d15035bee97ee6a773056ecf8b7
huangyijie <huang.yijie@iwhalecloud.com>
2023-03-19 15:48:49 +0800

    20230319 项目初始化

---
+++ src/main/java/com/ateh/eh/EasyHelpApplication.java
@@ -0,0 +1,13 @@
+package com.ateh.eh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyHelpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyHelpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ateh/eh/EasyHelpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ateh.eh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyHelpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
